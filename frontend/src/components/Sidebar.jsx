e592ec2304a048cacc176a23c9a0dd9e08fbde26
Alberto Araújo <albertoafaj@hotmail.com>
2023-05-11 15:08:55 -0300

    feat: implement sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,93 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+import logo from '../assets/logo.png';
+import { useSelector } from 'react-redux';
 
-const Sidebar = () => {
+const Sidebar = ({ closeToogle }) => {
+  const { user } = useSelector((state) => state);
+  const { image, id, name } = user.data;
+  const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+  const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+  const handleCloseSidebar = () => {
+    if (closeToogle) closeToogle(false);
+  };
+  const categories = [
+    {
+      id: 1,
+      name: 'Animais',
+      friendlyURL: 'animais',
+    },
+    {
+      id: 2,
+      name: 'Papéis de Parede',
+      friendlyURL: 'papeis-de-parede',
+    },
+    {
+      id: 3,
+      name: 'Fotos',
+      friendlyURL: 'fotos',
+    },
+    {
+      id: 4,
+      name: 'Jogos',
+      friendlyURL: 'jogos',
+    },
+    {
+      id: 5,
+      name: 'Codificação',
+      friendlyURL: 'codificacao',
+    },
+    {
+      id: 6,
+      name: 'Outros',
+      friendlyURL: 'outros',
+    },
+  ];
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col justify-between bg-white overflow-y-scrikk min-w-2010 hide-scrollbar'>
+      <div className="flex flex-col"></div>
+      <Link
+        to="/"
+        className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+        onClick={handleCloseSidebar}>
+        <img src={logo} alt="logo" className='w-full' />
+      </Link>
+      <div className='flex flex-col gap-5'>
+        <NavLink
+          to='/'
+          className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+          onClick={handleCloseSidebar}
+        >
+          <RiHomeFill />
+          Home
+        </NavLink>
+        <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Descubra categorias</h3>
+        {categories.map((category) => {
+          const { id, name, friendlyURL } = category;
+          return <NavLink
+            to={`/categorias/${friendlyURL}`}
+            className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+            onClick={handleCloseSidebar}
+            key={id}
+          >
+            {name}
+          </NavLink>
+
+        })}
+      </div>
+      {id && (
+        <Link
+          to={`perfil-do-usuario/${id}`}
+          className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
+          onClick={handleCloseSidebar}
+        >
+          <img src={image} className="w-10 h-10 rounded-full" alt='perfil-do-usuario' />
+          <p>{name}</p>
+        </Link>
+      )}
+    </div>
   )
 }
 
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
@@ -8,16 +8,18 @@ import Pins from './Pins';
 import logo from '../assets/logo.png';
 
 const Home = () => {
+
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const { token, user } = useSelector((state) => state);
-  const userData = user.data;
+  const { image, id } = user.data;
+
   const navigate = useNavigate();
   const scrollRef = useRef(null);
   React.useEffect(() => {
     if (!token.data) {
       navigate('/login');
     }
-  }, [token]);
+  }, [token, navigate]);
   React.useEffect(() => {
     scrollRef.current.scrollTo(0, 0);
   }, []);
@@ -25,7 +27,7 @@ const Home = () => {
     <>
       <div className="flex bg-gray-50 md:flex-fow flex-col h-screen transaction-height duration-75 ease-out">
         <div className="hidden md:flex h-screen flex-initial">
-          <Sidebar userData={userData && userData} closeToggle={setToggleSidebar} />
+          <Sidebar closeToggle={setToggleSidebar} />
         </div>
         <div className="flex md:hidden flex-row">
           <div className='p-2 w-full flex justify-between items-center shadow-md'>
@@ -33,8 +35,8 @@ const Home = () => {
             <Link to="/">
               <img src={logo} alt="logo" className='w-28' />
             </Link>
-            <Link to={`user-profile/${userData?.id}`}>
-              <img src={userData?.image} alt="logo" className='w-28' />
+            <Link to={`user-profile/${id}`}>
+              <img src={image} alt="logo" className='w-28' />
             </Link>
           </div>
           {toggleSidebar && (
@@ -42,14 +44,14 @@ const Home = () => {
               <div className="absolute w-full flex justify-end items-center p-2">
                 <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
               </div>
-              <Sidebar userData={userData && userData} closeToggle={setToggleSidebar} />
+              <Sidebar closeToggle={setToggleSidebar} />
             </div>
           )}
         </div>
         <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
           <Routes>
             <Route path="/user-profile/:userId" element={<UserProfile />} />
-            <Route path="/*" element={<Pins user={userData && userData} />} />
+            <Route path="/*" element={<Pins />} />
           </Routes>
         </div>
       </div >
