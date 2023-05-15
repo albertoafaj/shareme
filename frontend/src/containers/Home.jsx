44b0fda17e94daed15de4c52a59b08f04efeace5
Alberto Araújo <albertoafaj@hotmail.com>
2023-05-15 14:21:21 -0300

    fix: check for user data before component is mounted

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { Sidebar, UserProfile } from '../components';
 import Pins from './Pins';
 import logo from '../assets/logo.png';
@@ -11,50 +11,55 @@ const Home = () => {
 
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const { token, user } = useSelector((state) => state);
-  const { image, id } = user.data;
-
+  const { data: userData } = user;
   const navigate = useNavigate();
   const scrollRef = useRef(null);
   React.useEffect(() => {
     if (!token.data) {
       navigate('/login');
     }
-  }, [token, navigate]);
+  }, [token.data, navigate]);
   React.useEffect(() => {
-    scrollRef.current.scrollTo(0, 0);
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo(0, 0);
+    }
   }, []);
   return (
     <>
-      <div className="flex bg-gray-50 md:flex-fow flex-col h-screen transaction-height duration-75 ease-out">
-        <div className="hidden md:flex h-screen flex-initial">
-          <Sidebar closeToggle={setToggleSidebar} />
-        </div>
-        <div className="flex md:hidden flex-row">
-          <div className='p-2 w-full flex justify-between items-center shadow-md'>
-            <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
-            <Link to="/">
-              <img src={logo} alt="logo" className='w-28' />
-            </Link>
-            <Link to={`user-profile/${id}`}>
-              <img src={image} alt="logo" className='w-28' />
-            </Link>
+      {userData?.id !== undefined &&
+
+        <div className="flex bg-gray-50 md:flex-fow flex-col h-screen transaction-height duration-75 ease-out">
+          <div className="hidden md:flex h-screen flex-initial">
+            <Sidebar closeToggle={setToggleSidebar} />
           </div>
-          {toggleSidebar && (
-            <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-              <div className="absolute w-full flex justify-end items-center p-2">
-                <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
-              </div>
-              <Sidebar closeToggle={setToggleSidebar} />
+          <div className="flex md:hidden flex-row">
+            <div className='p-2 w-full flex justify-between items-center shadow-md'>
+              <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
+              <Link to="/">
+                <img src={logo} alt="logo" className='w-28' />
+              </Link>
+              <Link to={`user-profile/${userData?.id}`}>
+                <img src={userData?.image} alt="logo" className='w-28' />
+              </Link>
             </div>
-          )}
-        </div>
-        <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
-          <Routes>
-            <Route path="/user-profile/:userId" element={<UserProfile />} />
-            <Route path="/*" element={<Pins />} />
-          </Routes>
-        </div>
-      </div >
+            {toggleSidebar && (
+              <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                <div className="absolute w-full flex justify-end items-center p-2">
+                  <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+                </div>
+                <Sidebar closeToggle={setToggleSidebar} />
+              </div>
+            )}
+          </div>
+          <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+            <Routes>
+              <Route path="/user-profile/:userId" element={<UserProfile />} />
+              <Route path="/*" element={<Pins />} />
+            </Routes>
+          </div>
+        </div >
+      }
+
     </>
   )
 }
