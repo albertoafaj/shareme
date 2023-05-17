c61f0f9222178c30e0616630011ab8b41ef3e241
Alberto Araújo <albertoafaj@hotmail.com>
2023-05-17 13:48:00 -0300

    feat: add search term prop

--- frontend/src/containers/Pins.jsx
+++ frontend/src/containers/Pins.jsx
@@ -3,10 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar, Search } from '../components';
 
 const Pins = () => {
+  const [searchTerm, setSearchTerm] = React.useState(null);
   return (
     <>
       <h1>Pins</h1>
-      <Navbar></Navbar>
+      <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} ></Navbar>
       <div className='h-full'>
         <Routes>
           <Route path='search' element={<Search />} />
