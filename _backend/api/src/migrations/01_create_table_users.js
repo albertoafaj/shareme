c4ce1a1d534010f6d18adfa6cc02dfb937539945
Alberto Araújo <albertoafaj@hotmail.com>
2023-06-07 09:17:31 -0300

    refactor: change name field to not allow null data

--- _backend/api/src/migrations/01_create_table_users.js
+++ _backend/api/src/migrations/01_create_table_users.js
@@ -1,6 +1,6 @@
 exports.up = (knex) => knex.schema.createTable('users', (t) => {
   t.increments('id').primary();
-  t.string('name', 255);
+  t.string('name', 255).notNull();
   t.string('email', 255).notNull().unique();
   t.string('passwd').notNull();
   t.string('image').notNull();
